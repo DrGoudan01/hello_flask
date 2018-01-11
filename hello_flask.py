2d36576a981904be7ecf55d3f1e0b6ef5b81d0ba
DrGoudan01 <18813290224@163.com>
2018-01-11 19:25:46 +0800

    v0.1 需要修改的地方： 将方法转为class index输出没有url跳转 需要增加的地方： 每个单独页面的路由

--- hello_flask.py
+++ hello_flask.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask,render_template
+import json
+import os
+
+
+
 
 app = Flask(__name__)
+#自动reload
+app.config['TEMPLATES_AUTO_RELOAD']=True
+
+def get_file():
+    filenames=os.listdir('E:\\hello_flask\\files')
+    titiles={}
+    for Filename in filenames:
+        with open('E:\\hello_flask\\files\\'+Filename,'r') as file :
+            tt=json.loads(file.read())
+            #输出{'1': 'Linux', '2': 'Vim', '3': 'Git'}
+        titiles[Filename]=tt
+    return titiles
+#return type {aaa:{aaaa:xxx},}
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    file=get_file()
+    filenames={}
+    for x in file.keys():
+        filenames[x]=file[x]['title']
 
+
+    return render_template('index.html',titiles=filenames)
 
 if __name__ == '__main__':
+    print(get_file())
+
     app.run()
+
